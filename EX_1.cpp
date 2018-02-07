742df24dc1adb7e796ae4d7a25e58f8a27b56d95
Aluno <aluno@udicentro.local>
2018-02-07 20:38:55 -0200

    Quarto Commit

--- EX_1.cpp
+++ EX_1.cpp
@@ -1,21 +1,21 @@
-<stdio.h>
+#include<stdio.h>
 
 
-void main (){
+int main (){
 	 int numero_1 = 0;
 	 int numero_2 = 0;
 	 int numero_3 = 0;
 	 int numero_4 = 0;
 	 
-	 scanf("%d",&numero_1);
-	 scanf("%d",&numero_2);
-	 scanf("%d",&numero_3);
-	 scanf("%d",&numero_4);
+	 scanf("%d\n",&numero_1);
+	 scanf("%d\n",&numero_2);
+	 scanf("%d\n",&numero_3);
+	 scanf("%d\n",&numero_4);
 	 
-	 if(((numero_1+numero_2+numero_3+numero_4)\4)<6){
-	 	prinf("Reprovado");
+	 if(((numero_1+numero_2+numero_3+numero_4)/4)<6){
+	 	printf("Reprovado");
 	 }else{
-	 	prinf("Aprovado");
+	 	printf("Aprovado");
 	 }
 	 	 	 	 
 	
---
+++ EX_3.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+int main (){
+
+for(int i =299; i>100;i--){
+	 int mod= i%2;
+	 
+	 if(mod == 1)
+	 printf("%i\n", i);
+	
+}
+
+
+}
