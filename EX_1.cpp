4c9d1408e10181dd982f3df6ec16ea4d487ea926
Aluno <aluno@udicentro.local>
2018-02-07 20:19:16 -0200

    Primeiro commit

---
+++ EX_1.cpp
@@ -0,0 +1,23 @@
+<stdio.h>
+
+
+void main (){
+	 int numero_1 = 0;
+	 int numero_2 = 0;
+	 int numero_3 = 0;
+	 int numero_4 = 0;
+	 
+	 scanf("%d",&numero_1);
+	 scanf("%d",&numero_2);
+	 scanf("%d",&numero_3);
+	 scanf("%d",&numero_4);
+	 
+	 if(((numero_1+numero_2+numero_3+numero_4)\4)<6){
+	 	prinf("Reprovado");
+	 }else{
+	 	prinf("Aprovado");
+	 }
+	 	 	 	 
+	
+	
+}
